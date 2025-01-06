0e76c335e74b6a95a566362258423f62b5a29570
camtmsmith <cam.tm.smith@gmail.com>
2025-01-06 18:23:16 +1000

    1.1.4

--- Arduino_Visual-Basic/Form1.Designer.vb
+++ Arduino_Visual-Basic/Form1.Designer.vb
@@ -480,7 +480,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Location = New System.Drawing.Point(749, 131)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(39, 13)
         Me.Label1.TabIndex = 72
